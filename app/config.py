bf5473afa4255a50abd4620aa988e0164b3c3efd
Pedro <ppplayback68@gmail.com>
2025-06-23 16:39:45 -0300

    printa a SECRET_KEY

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Pega o caminho absoluto do arquivo .env na pasta raiz (uma pasta acima de app/)
 env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
