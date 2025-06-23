9b13247872914e66ca760cf53eef46df44e44cd2
Pedro <ppplayback68@gmail.com>
2025-06-23 16:34:12 -0300

    Configura carregamento da SECRET_KEY via .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask, Blueprint
-from app.config import senha_secreta
+from dotenv import load_dotenv
+from pathlib import Path
+from flask import Flask
+import os
+
 from app.routes.home import home_route
 from app.processor import upload_route
 from app.processor import download_image_route
 from app.routes.cadastro.criar_conta import sing_up_route, email_confirm_route
 from app.routes.cadastro.login import login_route
 
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
 app = Flask(__name__)
-app.secret_key = senha_secreta()
+app.secret_key = os.getenv('SECRET_KEY')
 
 #registrando Blueprint
 app.register_blueprint(home_route)
--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-def senha_secreta():
-    return 'PP10*10@2007%OC'
+# Pega o caminho absoluto do arquivo .env na pasta raiz (uma pasta acima de app/)
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
+print(os.getenv('SECRET_KEY'))
